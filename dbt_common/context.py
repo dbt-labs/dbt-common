75a8524c2326358b95c348e22d850c3ee80d5a14
Peter Allen Webb <peter.webb@dbtlabs.com>
2024-01-30 11:03:09 -0500

    Add new invocation context class.

---
+++ dbt_common/context.py
@@ -0,0 +1,36 @@
+from contextvars import ContextVar
+import os
+from typing import List, Mapping
+
+from dbt_common.constants import SECRET_ENV_PREFIX
+
+
+class InvocationContext:
+    def __init__(self):
+        self._env: Mapping[str, str] = None
+        self._env_secrets: List[str]
+        # This class will also eventually manage the invocation_id, flags, event manager, etc.
+
+    @property
+    def env(self) -> Mapping[str, str]:
+        if self._env is None:
+            self._env = os.environ
+
+        return self._env
+
+    @property
+    def env_secrets(self) -> List[str]:
+        return [v for k, v in self.env.items() if k.startswith(SECRET_ENV_PREFIX) and v.strip()]
+
+
+
+_INVOCATION_CONTEXT_VAR: ContextVar[InvocationContext] = ContextVar("DBT_INVOCATION_CONTEXT_VAR")
+
+
+def set_invocation_context() -> None:
+    _INVOCATION_CONTEXT_VAR.set(InvocationContext())
+
+
+def get_invocation_context() -> InvocationContext:
+    ctx = _INVOCATION_CONTEXT_VAR.get()
+    return ctx

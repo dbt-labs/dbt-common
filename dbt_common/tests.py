246c4bcf855e245eaf31ea4818e49c59598f5793
Peter Allen Webb <peter.webb@dbtlabs.com>
2024-02-01 10:56:39 -0500

    Add method to disable test caching.

--- dbt_common/tests.py
+++ dbt_common/tests.py
@@ -8,3 +8,8 @@ def test_caching_enabled() -> bool:
 def enable_test_caching() -> None:
     global _TEST_CACHING_ENABLED
     _TEST_CACHING_ENABLED = True
+
+
+def disable_test_caching() -> None:
+    global _TEST_CACHING_ENABLED
+    _TEST_CACHING_ENABLED = False

cfcb8fb6ed94a1e2b79c7a189ff254c8e6e6204a
Peter Allen Webb <peter.webb@dbtlabs.com>
2024-02-01 12:24:50 -0500

    Fix string constant.

--- tests/unit/test_invocation_context.py
+++ tests/unit/test_invocation_context.py
@@ -12,8 +12,8 @@ def test_invocation_context_secrets():
     test_env = {
         f"{SECRET_ENV_PREFIX}_VAR_1": "secret1",
         f"{SECRET_ENV_PREFIX}VAR_2": "secret2",
-        f"NON_SECRET": "nonsecret",
-        f"foo{SECRET_ENV_PREFIX}": "nonsecret",
+        "NON_SECRET": "non-secret",
+        f"foo{SECRET_ENV_PREFIX}": "non-secret",
     }
     ic = InvocationContext(env=test_env)
     assert set(ic.env_secrets) == set(["secret1", "secret2"])

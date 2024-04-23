87a7cc5e56d4c0e8ac2c0c084943b42c79a7d9c5
Emily Rockman <ebuschang@gmail.com>
2024-04-23 13:01:12 -0500

    remove final underscore (#110)
    * remove final underscore
    
    * changelog

--- dbt_common/constants.py
+++ dbt_common/constants.py
@@ -1,6 +1,6 @@
 # Prefix which identifies environment variables which contains secrets.
-SECRET_ENV_PREFIX = "DBT_ENV_SECRET_"
+SECRET_ENV_PREFIX = "DBT_ENV_SECRET"
 
 # Prefix which identifies environment variables that should not be visible
 # via macros, flags, or other user-facing mechanisms.
-PRIVATE_ENV_PREFIX = "DBT_ENV_PRIVATE_"
+PRIVATE_ENV_PREFIX = "DBT_ENV_PRIVATE"

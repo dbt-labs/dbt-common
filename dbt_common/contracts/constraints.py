35c6bfc26f60af2d4ca8e55f8bc820ae19d42a9a
Michelle Ark <michelle.ark@dbtlabs.com>
2024-07-16 10:20:30 -0400

    linting

--- dbt_common/contracts/constraints.py
+++ dbt_common/contracts/constraints.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from enum import Enum
 from typing import Optional, List
 
-from dbt_common.dataclass_schema import dbtClassMixin, ValidationError
+from dbt_common.dataclass_schema import dbtClassMixin
 
 
 class ConstraintType(str, Enum):

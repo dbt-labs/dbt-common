3c8d30d71b44cf26611dbf7e7a05d1318037bd82
Vengal Rao Guttha <vengalrao.guttha@snowflake.com>
2025-03-06 10:35:04 -0800

    refactor

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -1,3 +1,4 @@
+import pytest
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -18,7 +19,9 @@ class TestConfig:
         self.threads = threads
 
 
-def test_multi_threaded_executor_opentelemetry_context_propagation():
+@pytest.mark.parametrize("single_threaded, threads", [(False, 2), (True, 1)])
+# Note: In single thread mode, it is executed on current thread. So no need to pass context explicitly
+def test_executor_opentelemetry_context_propagation(single_threaded, threads):
     tracer_provider = TracerProvider(resource=Resource.get_empty())
     span_exporter = InMemorySpanExporter()
     trace.set_tracer_provider(tracer_provider)
@@ -26,28 +29,7 @@ def test_multi_threaded_executor_opentelemetry_context_propagation():
     tracer = trace.get_tracer("test.dbt.runner")
     set_invocation_context({})
     with tracer.start_as_current_span("test-span") as span:
-        config = TestConfig(ThreadingType(single_threaded=False), threads=2)
-
-        def func():
-            return trace.get_current_span().get_span_context()
-
-        with executor(config) as ex:
-            future = ex.submit(func)
-            span_context_in_thread = future.result()
-            assert span.get_span_context().trace_id == span_context_in_thread.trace_id
-            assert span.get_span_context().span_id == span_context_in_thread.span_id
-
-
-# In single thread mode, it is executed on current thread. So no need to pass context explicitly
-def test_single_threaded_executor_opentelemetry_context_propagation():
-    tracer_provider = TracerProvider(resource=Resource.get_empty())
-    span_exporter = InMemorySpanExporter()
-    trace.set_tracer_provider(tracer_provider)
-    trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(span_exporter))
-    tracer = trace.get_tracer("test.dbt.runner")
-    set_invocation_context({})
-    with tracer.start_as_current_span("test-span") as span:
-        config = TestConfig(ThreadingType(single_threaded=True))
+        config = TestConfig(ThreadingType(single_threaded=single_threaded), threads=threads)
 
         def func():
             return trace.get_current_span().get_span_context()

e9efd8343ce4dd9e694814716a115cab27e8bcf7
Quigley Malcolm <QMalcolm@users.noreply.github.com>
2025-03-20 14:43:19 -0500

    Backwards Compatible Version of: " Update fire_event to handle warn_or_error logic #237" (#259)
    * Revert "Revert "Update `fire_event` to handle `warn_or_error` logic (#237)" (#258)"
    
    This reverts commit 0a93135caaa8c517480d47e1a836436f1e46ac7f.
    
    * Backwards Compat: use `WARN_ERROR_OPTIONS` and `WARN_ERROR` from functions module as defaults
    
    In dbt-core <= 1.9, settings for `warn_error_options` and `warn_error` is handled by
    the `functions` module properties `WARN_ERROR_OPTIONS` and `WARN_ERROR` respectively.
    As we don't want this upgrade of warn_or_error handling to be a breaking change, we
    need to support the old way of doing things. This makes it such that the `warn_error_options`
    and `warn_error` properties of the `EventManager` class will, if not set,  default to the
    `function` module properties. In dbt-core 1.10+, the properties on the `EventManger` will instead
    be set directly.
    
    * Back compat: ability to handle non-warning type events with warn_or_error logic
    
    Previously if you sent a event through `warn_or_error`, no matter it's level it
    would do the `warn_or_error` stuff (silencing, making it an error, etc). Of note
    at present only _two_ non-warning type events are sent through `warn_or_error`. To
    continue supporting this we've altered the logic for gating `warn_or_error`
    to do `warn_or_error` handling IFF either the following is true:
    1. `fire_event` call with `force_warn_or_error_handling=True`
    2. `require_warn_or_error_handling=True` AND `event.level="warn"`
    
    With this logic, we'll be able to move to a future where all warnings will
    be eventually handled by `warn_or_error` logic by default AND other event
    types can be opted in to be handled by `warn_or_error` by passing an additional
    arg to `fire_event`.
    
    * Update `TestEventManager` to support new `IEventManager` properties and `fire_event` args

--- dbt_common/events/event_manager.py
+++ dbt_common/events/event_manager.py
@@ -1,18 +1,65 @@
 import os
 import traceback
-from typing import List, Optional, Protocol, Tuple
+from typing import Any, List, Optional, Protocol, Tuple
 
 from dbt_common.events.base_types import BaseEvent, EventLevel, msg_from_base_event, TCallback
 from dbt_common.events.logger import LoggerConfig, _Logger, _TextLogger, _JsonLogger, LineFormat
+from dbt_common.exceptions.events import EventCompilationError
+from dbt_common.helper_types import WarnErrorOptions
 
 
 class EventManager:
     def __init__(self) -> None:
         self.loggers: List[_Logger] = []
         self.callbacks: List[TCallback] = []
+        self._warn_error: Optional[bool] = None
+        self._warn_error_options: Optional[WarnErrorOptions] = None
+        self.require_warn_or_error_handling: bool = False
 
-    def fire_event(self, e: BaseEvent, level: Optional[EventLevel] = None) -> None:
+    @property
+    def warn_error(self) -> bool:
+        if self._warn_error is None:
+            from dbt_common.events.functions import WARN_ERROR
+
+            return WARN_ERROR
+        return self._warn_error
+
+    @warn_error.setter
+    def warn_error(self, warn_error: bool) -> None:
+        self._warn_error = warn_error
+
+    @property
+    def warn_error_options(self) -> WarnErrorOptions:
+        if self._warn_error_options is None:
+            from dbt_common.events.functions import WARN_ERROR_OPTIONS
+
+            return WARN_ERROR_OPTIONS
+        return self._warn_error_options
+
+    @warn_error_options.setter
+    def warn_error_options(self, warn_error_options: WarnErrorOptions) -> None:
+        self._warn_error_options = warn_error_options
+
+    def fire_event(
+        self,
+        e: BaseEvent,
+        level: Optional[EventLevel] = None,
+        node: Any = None,
+        force_warn_or_error_handling: bool = False,
+    ) -> None:
         msg = msg_from_base_event(e, level=level)
+
+        if force_warn_or_error_handling or (
+            self.require_warn_or_error_handling and msg.info.level == "warn"
+        ):
+            event_name = type(e).__name__
+            if self.warn_error or self.warn_error_options.includes(event_name):
+                # This has the potential to create an infinite loop if the handling of the raised
+                # EventCompilationError fires an event as a warning instead of an error.
+                raise EventCompilationError(e.message(), node)
+            elif self.warn_error_options.silenced(event_name):
+                # Return early if the event is silenced
+                return
 
         if os.environ.get("DBT_TEST_BINARY_SERIALIZATION"):
             print(f"--- {msg.info.name}")
@@ -48,8 +95,17 @@ class EventManager:
 class IEventManager(Protocol):
     callbacks: List[TCallback]
     loggers: List[_Logger]
+    warn_error: bool
+    warn_error_options: WarnErrorOptions
+    require_warn_or_error_handling: bool
 
-    def fire_event(self, e: BaseEvent, level: Optional[EventLevel] = None) -> None:
+    def fire_event(
+        self,
+        e: BaseEvent,
+        level: Optional[EventLevel] = None,
+        node: Any = None,
+        force_warn_or_error_handling: bool = False,
+    ) -> None:
         ...
 
     def add_logger(self, config: LoggerConfig) -> None:
@@ -65,8 +121,17 @@ class TestEventManager(IEventManager):
     def __init__(self) -> None:
         self.event_history: List[Tuple[BaseEvent, Optional[EventLevel]]] = []
         self.loggers = []
+        self.warn_error = False
+        self.warn_error_options = WarnErrorOptions(include=[], exclude=[])
+        self.require_warn_or_error_handling = False
 
-    def fire_event(self, e: BaseEvent, level: Optional[EventLevel] = None) -> None:
+    def fire_event(
+        self,
+        e: BaseEvent,
+        level: Optional[EventLevel] = None,
+        node: Any = None,
+        force_warn_or_error_handling: bool = False,
+    ) -> None:
         self.event_history.append((e, level))
 
     def add_logger(self, config: LoggerConfig) -> None:

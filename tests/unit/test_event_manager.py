81a3df95500807fcc7d1f5b041ef44cf0d6a61b9
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-04-18 09:50:36 -0700

    Ensure `fire_event` allows for the silencing of deprecations

--- tests/unit/test_event_manager.py
+++ tests/unit/test_event_manager.py
@@ -1,4 +1,5 @@
 from dbt_common.events.event_manager import EventManager
+from dbt_common.events.types import BehaviorChangeEvent
 from tests.unit.utils import EventCatcher
 
 
@@ -9,3 +10,25 @@ class TestEventManager:
 
         event_manager.add_callback(EventCatcher().catch)
         assert len(event_manager.callbacks) == 1
+
+
+class TestEventManagerSilencedDeprecation:
+    def test_can_silenced_deprecation_event(self) -> None:
+        event_catcher = EventCatcher()
+        event_manager = EventManager()
+        event_manager.add_callback(event_catcher.catch)
+
+        event = BehaviorChangeEvent(
+            flag_name="test",
+            flag_source="test",
+            description="test",
+            docs_url="test",
+        )
+
+        event_manager.fire_event(e=event, force_warn_or_error_handling=True)
+        assert len(event_manager.callbacks) == 1
+
+        event_catcher.caught_events.clear()
+        event_manager.warn_error_options.silence.append("Deprecations")
+        event_manager.fire_event(e=event, force_warn_or_error_handling=True)
+        assert len(event_catcher.caught_events) == 0

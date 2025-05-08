0da6a6776ba55ff1ba8b491b747d81c864a6b14e
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-05-08 16:56:28 -0500

    Optionally put event name in message tags

--- tests/unit/test_ui.py
+++ tests/unit/test_ui.py
@@ -1,11 +1,25 @@
-from dbt_common.ui import warning_tag, error_tag
+from dbt_common.ui import deprecation_tag, warning_tag, error_tag
 
 
 def test_warning_tag() -> None:
     tagged = warning_tag("hi")
-    assert "WARNING" in tagged
+    assert tagged == "[\x1b[33mWARNING\x1b[0m]: hi"
+
+    tagged = warning_tag("hi", "MyWarningEvent")
+    assert tagged == "[\x1b[33mWARNING\x1b[0m][MyWarningEvent]: hi"
 
 
 def test_error_tag() -> None:
     tagged = error_tag("hi")
-    assert "ERROR" in tagged
+    assert tagged == "[\x1b[31mERROR\x1b[0m]: hi"
+
+    tagged = error_tag("hi", "MyErrorEvent")
+    assert tagged == "[\x1b[31mERROR\x1b[0m][MyErrorEvent]: hi"
+
+
+def test_deprecation_tag() -> None:
+    tagged = deprecation_tag("hi")
+    assert tagged == "[\x1b[33mWARNING\x1b[0m]: Deprecated functionality\n\nhi"
+
+    tagged = deprecation_tag("hi", "MyDeprecationEvent")
+    assert tagged == "[\x1b[33mWARNING\x1b[0m][MyDeprecationEvent]: Deprecated functionality\n\nhi"

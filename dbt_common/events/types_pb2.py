d0b69f11707d61b6154fcc9ec8c8236e148b23da
Michelle Ark <michelle.ark@dbtlabs.com>
2025-05-12 19:18:20 -0400

    do that properly

--- dbt_common/events/types_pb2.py
+++ dbt_common/events/types_pb2.py
@@ -1,2 +1,2 @@
 # preserving import path during dbtlabs.proto refactor
-from dbtlabs.proto.public.v1.fields import common_types_pb2 # noqa
+from dbtlabs.proto.public.v1.fields.common_types_pb2 import * # noqa

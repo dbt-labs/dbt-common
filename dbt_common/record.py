def30cea41aa9a4bd27fead60d72bfc10df78792
Michelle Ark <MichelleArk@users.noreply.github.com>
2025-09-24 13:42:08 -0400

    Record-mode: Fallback more reliably to constructor-based paramater instantiation (#314)

--- dbt_common/record.py
+++ dbt_common/record.py
@@ -209,9 +209,9 @@ class Recorder:
                     dct = Recorder._get_tagged_dict(record, rec_cls_name)
                     json.dump(dct, self._recording_file)
                     self._record_added = True
-                except Exception:
+                except Exception as e:
                     json.dump(
-                        {"type": "RecordingError", "record_type": rec_cls_name},
+                        {"type": "RecordingError", "record_type": rec_cls_name, "error": str(e)},
                         self._recording_file,
                     )
         else:
@@ -539,23 +539,19 @@ def _record_function_inner(
         # changes the signature of the base recorded method, and so is wrapped in a try/except.
         params = None
         try:
-            # Omits any additional properties that are not fields of the params class
-            params_dict = {
-                field.name: value
-                for field, value in zip(dataclasses.fields(record_type.params_cls), param_args)
-            }
-            params_dict.update(kwargs)
             try:
+                # Omits any additional properties that are not fields of the params class
+                params_dict = {
+                    field.name: value
+                    for field, value in zip(dataclasses.fields(record_type.params_cls), param_args)
+                }
+                params_dict.update(kwargs)
                 params = record_type.params_cls._from_dict(params_dict)
-            except (TypeError, AttributeError):
-                params = record_type.params_cls(*param_args, **kwargs)
             except Exception:
-                # Unfortunately it is not possible to fire an event here because it would cause a circular import
-                # This means we lose visibility into the issue, but it is better than crashing the entire node or command
-                pass
+                params = record_type.params_cls(*param_args, **kwargs)
         except Exception:
             # Unfortunately it is not possible to fire an event here because it would cause a circular import
-            # This means we lose visibility into the issue, but it is better than crashing the entire node or command
+            # This means we lose visibility into issues using record_type.params_cls(...), but it is better than crashing the entire node or command
             pass
 
         include = True

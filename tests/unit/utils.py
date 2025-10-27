e406ebbcfc4598a75695c3dcc3e7c1d4036e9662
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-27 11:49:23 -0500

    Move `EventCatcher` to be part of the `events` lib of the distributed package
    Previously `EventCatcher` was only defined in `tests` which doesn't get distributed
    as part of the `dbt-common` package. We also have an `EventCatcher` defined in
    dbt-core's test suite, notably this definition has some features added to it. We also
    now find ourselves needing an `EventCatcher` in dbt-adapters. As such it makes sense
    to start including an `EventCatcher` as part of the dbt-common package.

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -1,20 +1,9 @@
-from dataclasses import dataclass, field
-from typing import List
-
 import pytest
 from pytest_mock import MockerFixture
 
-from dbt_common.events.base_types import EventMsg
+from dbt_common.events.event_catcher import EventCatcher
 from dbt_common.events.event_manager import EventManager
 from dbt_common.events.event_manager_client import add_callback_to_manager
-
-
-@dataclass
-class EventCatcher:
-    caught_events: List[EventMsg] = field(default_factory=list)
-
-    def catch(self, event: EventMsg) -> None:
-        self.caught_events.append(event)
 
 
 @pytest.fixture(scope="function")

aace34aa84a8b7efbc482c99434f7000d32d353f
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-27 13:22:45 -0500

    Improve `EventCatcher` to include `event_to_catch` and `predicate` filtering
    This was already done to the dbt-core `EventCatcher`. These updates are to
    include those improvements in the now packaged `EventCatcher` class.

--- dbt_common/events/event_catcher.py
+++ dbt_common/events/event_catcher.py
@@ -1,12 +1,21 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import Callable, List, Optional
 
-from dbt_common.events.base_types import EventMsg
+from dbt_common.events.base_types import BaseEvent, EventMsg
 
 
 @dataclass
 class EventCatcher:
+    event_to_catch: Optional[BaseEvent] = None
     caught_events: List[EventMsg] = field(default_factory=list)
+    predicate: Callable[[EventMsg], bool] = lambda event: True
 
-    def catch(self, event: EventMsg) -> None:
-        self.caught_events.append(event)
+    def _check_event_type(self, event: EventMsg) -> bool:
+        return self.event_to_catch is None or event.info.name == self.event_to_catch.__name__
+
+    def catch(self, event: EventMsg):
+        if self._check_event_type(event) and self.predicate(event):
+            self.caught_events.append(event)
+
+    def flush(self) -> None:
+        self.caught_events = []

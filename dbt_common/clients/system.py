272b3d3b60208d65c4bc3458334a2e33cb4952a1
Oliver Tosky <42260747+otosky@users.noreply.github.com>
2025-10-28 09:33:50 -0700

    fix: only evaluate modification time on matched local files (#235)
    * fix: only evaluate mtime on matched local files
    
    * chore: add changelog entry
    
    * doc: add comment to clarify order of operations
    
    Co-authored-by: Quigley Malcolm <QMalcolm@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Quigley Malcolm <QMalcolm@users.noreply.github.com>

--- dbt_common/clients/system.py
+++ dbt_common/clients/system.py
@@ -127,14 +127,19 @@ def find_matching(
             # if ignore_spec.match(relative_dir):
             #     continue
             for local_file in local_files:
+                # Sometimes temporary files with `.<random_characters>` are created, this filters
+                # them out so we don't hard fail during a race condition when the file disappears
+                # before we get to calculating its modification time
+                if not reobj.match(local_file):
+                    continue
+
                 absolute_path = os.path.join(current_path, local_file)
                 relative_path = os.path.relpath(absolute_path, absolute_path_to_search)
                 relative_path_to_root = os.path.join(relative_path_to_search, relative_path)
 
                 modification_time = os.path.getmtime(absolute_path)
-                if reobj.match(local_file) and (
-                    not ignore_spec or not ignore_spec.match_file(relative_path_to_root)
-                ):
+
+                if not ignore_spec or not ignore_spec.match_file(relative_path_to_root):
                     matching.append(
                         {
                             "searched_path": relative_path_to_search,
